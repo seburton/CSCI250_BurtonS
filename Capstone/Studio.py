01f58657074421c2d5735786daa07673d7492f00
twitt55 <103554650+twitt55@users.noreply.github.com>
2023-12-05 12:45:06 -0700

    Studio.py

--- Capstone/Studio.py
+++ Capstone/Studio.py
@@ -8,14 +8,14 @@ import Recording
 
 class Studio:
 
-    def __init__(self, Theremin, Button, Light, Recording):
+    def __init__(self, Theremin, Button, Light, recording):
         self.Theremin = Theremin
         self.Button = Button
         self.Light = Light
-        self.Recording = Recording
+        self.Recording = recording
         self.dataQ = queue.Queue()
 
-    def dataConsumer(self, dataQ, theremin, Recording):
+    def dataConsumer(self, dataQ, theremin, recording):
         #this consumer gets note frequencies from the queue and plays them on the buzzer
         self.dataQ = queue.Queue()
         buttonsave = 0
@@ -26,11 +26,11 @@ class Studio:
             if Button.read() == True or buttonsave == 1:
                 buttonsave = 1
                 Light.on()
-                Recording.createfile()
+                recording.createfile()
                 if Button.read() == True:
                     buttonsave = 0
-                    Recording.filenum += 1
-                    Recording.save()
+                    recording.filenum += 1
+                    recording.save()
                     Light.off()
             if toPlay == False:
                 print("Loop Broken")
@@ -39,7 +39,7 @@ class Studio:
                 theremin.playNote(toPlay)
         
     def playTheremin(self):
-        threading.Thread(target = self.dataConsumer, args = (self.dataQ,self.Theremin)).start()
+        threading.Thread(target = self.dataConsumer, args = (self.dataQ,self.Theremin, self.Recording)).start()
 
     def stopTheremin(self):
         self.dataQ.put(False)
