168c48f5c1c8531c066fc651eab872a263740585
twitt55 <103554650+twitt55@users.noreply.github.com>
2023-12-05 16:06:50 -0700

    Recording.py

--- Capstone/Recording.py
+++ Capstone/Recording.py
@@ -3,11 +3,11 @@ import Note as Note
 
 class Recording():
     def __init__(self, name):
-        self.song = np.array([])
+        self.song = []
         self.name = name
         self.filenum = 0
     def createfile(self, toplay):
-        self.song += toplay
+        self.song.append(toplay)
         
     def save(self):
         filename = 'saves/' + self.name + str(self.filenum)+'.npz'
